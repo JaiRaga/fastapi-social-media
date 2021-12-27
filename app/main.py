e50025608a6fb639c59e3c7af065e6674c95600b
JaiRaga <raga.j.santhosh@gmail.com>
2021-12-28 04:11:37 +0530

    created sqlalchmey queries for update and delete queries

--- app/main.py
+++ app/main.py
@@ -83,25 +83,39 @@ def new_post(post: Post, db: Session = Depends(get_db)):
 
 # Update Post
 @app.put("/posts/{id}")
-def update_post(id: int, post: Post):
+def update_post(id: int, post: Post, db: Session = Depends(get_db)):
   # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
 
   # updated_post = cursor.fetchone()
   # conn.commit()
 
-  if updated_post == None:
+  post_query = db.query(models.Post).filter(models.Post.id == id)
+
+  found_post = post_query.first()
+
+  if found_post == None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Post with id {id} does not exists.')
-  return {"posts": updated_post}
+
+  post_query.update(post.dict(), synchronize_session=False)
+
+  db.commit()
+  
+  return {"posts": post_query.first()}
 
 
 # Delete Post
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
+def delete_post(id: int, db: Session = Depends(get_db)):
   # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
   # deleted_post = cursor.fetchone()
   # conn.commit()
 
-  if deleted_post == None:
+  post = db.query(models.Post).filter(models.Post.id == id)
+
+  if post.first() == None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Post with id {id} does not exists.')
   
+  post.delete(synchronize_session=False)
+  db.commit()
+
   return Response(status_code=status.HTTP_204_NO_CONTENT)
