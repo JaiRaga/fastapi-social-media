6294c1104f17f283301dfb1edc50655f76f9055d
JaiRaga <raga.j.santhosh@gmail.com>
2022-02-05 23:23:45 +0530

    :sparkles: added authentication to the routes

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from typing import List
 from sqlalchemy.orm import Session
+
+from app import oauth2
 from .. import models, schemas, utils
 from ..database import engine, get_db
 
@@ -32,13 +34,12 @@ def get_post(id: int, db: Session = Depends(get_db)):
 
 # Create Post
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def new_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def new_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, (post.title, post.content, post.published))
   # new_post = cursor.fetchone()
   # conn.commit()
 
-  print(1, post)
-  print(2, post.dict())
+  print(current_user, current_user.email)
 
   new_post = models.Post(**post.dict())
   db.add(new_post)
@@ -49,7 +50,7 @@ def new_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
 
 # Update Post
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
 
   # updated_post = cursor.fetchone()
@@ -71,7 +72,7 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 
 # Delete Post
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, db: Session = Depends(get_db)):
+def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
   # deleted_post = cursor.fetchone()
   # conn.commit()
