2e59b0a19d46f98663befab413bdb20a4909f4bd
JaiRaga <raga.j.santhosh@gmail.com>
2022-02-06 20:01:00 +0530

    :sparkles: created get posts by current user route, and added checks before deleting and updating posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -19,6 +19,12 @@ def get_posts(db: Session = Depends(get_db)):
   posts = db.query(models.Post).all()
   return posts
 
+# Get Posts By the current user
+@router.get("/me", response_model=List[schemas.Post])
+def get_my_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+  posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+  return posts
+
 # Get Single Post
 @router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db)):
@@ -41,7 +47,7 @@ def new_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_us
 
   print(current_user, current_user.email)
 
-  new_post = models.Post(**post.dict())
+  new_post = models.Post(owner_id=current_user.id, **post.dict())
   db.add(new_post)
   db.commit()
   db.refresh(new_post)
@@ -50,7 +56,7 @@ def new_post(post: schemas.PostCreate, db: Session = Depends(get_db), current_us
 
 # Update Post
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
   # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""", (post.title, post.content, post.published, str(id)))
 
   # updated_post = cursor.fetchone()
@@ -58,12 +64,15 @@ def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)
 
   post_query = db.query(models.Post).filter(models.Post.id == id)
 
-  found_post = post_query.first()
+  post = post_query.first()
 
-  if found_post == None:
+  if post == None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Post with id {id} does not exists.')
+  
+  if post.owner_id != current_user.id:
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")  
 
-  post_query.update(post.dict(), synchronize_session=False)
+  post_query.update(updated_post.dict(), synchronize_session=False)
 
   db.commit()
   
@@ -77,12 +86,17 @@ def delete_post(id: int, db: Session = Depends(get_db), current_user: int = Depe
   # deleted_post = cursor.fetchone()
   # conn.commit()
 
-  post = db.query(models.Post).filter(models.Post.id == id)
+  post_query = db.query(models.Post).filter(models.Post.id == id)
 
-  if post.first() == None:
+  post = post_query.first()
+
+  if post == None:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Post with id {id} does not exists.')
+
+  if post.owner_id != current_user.id:
+    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to perform requested action")
   
-  post.delete(synchronize_session=False)
+  post_query.delete(synchronize_session=False)
   db.commit()
 
   return Response(status_code=status.HTTP_204_NO_CONTENT)
